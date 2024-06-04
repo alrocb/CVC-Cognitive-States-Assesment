455de3d6af03810df4cba41951597379de7c03f2
alrocb <alexroldanins@gmail.com>
2024-06-04 14:57:35 +0200

    Actualizar

--- PyschoPy Files/Stoop Task/stroop_lastrun.py
+++ PyschoPy Files/Stoop Task/stroop_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2023.2.3),
-    on febrero 22, 2024, at 11:04
+    on junio 04, 2024, at 14:52
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -106,7 +106,7 @@ def setupData(expInfo, dataDir=None):
     thisExp = data.ExperimentHandler(
         name=expName, version='',
         extraInfo=expInfo, runtimeInfo=None,
-        originPath='C:\\Users\\Alex\\Desktop\\CVC\\PyschoPy Files\\Stoop Task\\stroop_lastrun.py',
+        originPath='C:\\Users\\Alex\\Desktop\\CVC\\CVC-Cognitive-States-Assesment\\PyschoPy Files\\Stoop Task\\stroop_lastrun.py',
         savePickle=True, saveWideText=True,
         dataFileName=dataDir + os.sep + filename, sortColumns='time'
     )
